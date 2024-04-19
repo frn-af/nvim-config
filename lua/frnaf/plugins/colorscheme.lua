b28f613fc4c9aecd67d5c667c9089e4cd80c9306
frn-af <91farhanalfathra@gmail.com>
2024-04-19 13:46:31 +0700

    plugins: mini and lualine

--- lua/frnaf/plugins/colorscheme.lua
+++ lua/frnaf/plugins/colorscheme.lua
@@ -2,8 +2,7 @@ return {
    'decaycs/decay.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'decay'
-
+      vim.cmd.colorscheme 'dark-decay'
       vim.cmd.hi 'Comment gui=none'
     end,
   }
