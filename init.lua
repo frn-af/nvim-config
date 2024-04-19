ed68bce95bfaa12271f05f3198884cc31c8d4a02
frn-af <91farhanalfathra@gmail.com>
2024-04-19 10:33:04 +0700

    init

---
+++ init.lua
@@ -0,0 +1,1 @@
+require("frnaf.core.options")
---
+++ lua/frnaf/core/options.lua
@@ -0,0 +1,16 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtap = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
